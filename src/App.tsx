f0e6e62adcb1ee37ceee0202ec77ac7018ae3f89
lm omar <lmouniromar@gmail.com>
2025-02-26 10:47:18 +0100

    HelloWord component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import HelloWorld from './components/HelloWorld';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <HelloWorld />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
